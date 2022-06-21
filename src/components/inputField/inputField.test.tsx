2c15c349f00d74dc7a33b91cd893850a62b3d1e0
deniSwd <denis_swd@mail.ru>
2022-06-21 13:01:18 +0200

    21.06.2022 deniSwd was added  test for inputField.

---
+++ src/components/inputField/inputField.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
+import * as hooks from '../../store/hooks'
+import {InputField} from './inputField';
+import {ToDoObjectType} from '../../store/slices/tasksListSlice';
+
+describe('inputField', () => {
+  test('should input field works', async () => {
+    const useDispatchSpy = jest.spyOn(hooks, 'useAppDispatch');
+    const mockDispatchFn = jest.fn()
+    useDispatchSpy.mockReturnValue(mockDispatchFn);
+
+    render(<InputField/>)
+
+    const input = screen.getByRole('textbox')
+    await userEvent.type(input, 'test{enter}')
+
+    // expect(dispatchMock).toBeCalled()
+    expect(mockDispatchFn).toHaveBeenCalled()
+    expect(mockDispatchFn).toHaveBeenCalledWith({
+      type: 'taskList/addTask',
+      payload: {
+        value: 'test',
+        isCompleted: false
+      } as ToDoObjectType
+    })
+  })
+})
